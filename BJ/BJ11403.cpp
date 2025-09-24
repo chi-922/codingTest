c3d76e1657d5553e031cbf5558fb318d3a90444a
임채현 <chi_927@naver.com>
2025-09-24 23:46:16 +0900

    9월 24일

---
+++ BJ/BJ11403.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int g[101][101];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> g[i][j];
+		}
+	}
+
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (g[i][k] && g[k][j]) {
+					g[i][j] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << g[i][j] << " ";
+		}
+		cout << '\n';
+	}
+
+}
